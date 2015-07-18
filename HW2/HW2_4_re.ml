0d4c1fdab717a20220f361eae01b18dd647e2f50
postBG <profile2697@gmail.com>
2015-07-18 00:39:01 -0700

    second commit with error repaired

--- HW2/HW2_4_re.ml
+++ HW2/HW2_4_re.ml
@@ -10,25 +10,27 @@ type exp = X
 	| SIGMA of exp * exp * exp
 	| INTEGRAL of exp * exp * exp
 
-let rec eval (exp, num) =
-	match exp with
-	| X -> num
-	| INT n -> float_of_int n
-	| REAL n -> n
-	| ADD(e1, e2) -> eval(e1, num) +. eval(e2, num)
-	| SUB(e1, e2) -> eval(e1, num) -. eval(e2, num)
-	| MUL(e1, e2) -> eval(e1, num) *. eval(e2, num)
-	| DIV(e1, e2) -> eval(e1, num) /. eval(e2, num)
-	| SIGMA(e1, e2, e3) -> eval_sigma(e3, int_of_float eval(e1, num), int_of_float eval(e2, num))
-	| INTEGRAL(e1, e2, e3) -> eval_integral(e3, eval(e1, num), eval(e2, num))
-and eval_sigma (exp, n, end) =
-	if(n > end) then 0.0
-	else eval(exp, float_of_int n) +. eval_sigma(exp, n+1, end)
-and eval_integral (exp, n, end) =
-	if(n > end) then -1.0 *. eval_integral(exp, end, n)
-	else if(end -. n < 0.1) then 0.0
-	else (0.1 *. eval(exp, n)) +. eval_integral(exp, n+0.1, end)
+let rec eval (expr, num) =
+	match expr with
+		| X -> num
+		| INT n -> float_of_int n
+		| REAL n -> n
+		| ADD (e1, e2) -> eval(e1, num) +. eval(e2, num)
+		| SUB (e1, e2) -> eval(e1, num) -. eval(e2, num)
+		| MUL (e1, e2) -> eval(e1, num) *. eval(e2, num)
+		| DIV (e1, e2) -> eval(e1, num) /. eval(e2, num)
+		| SIGMA (e1, e2, e3) -> eval_sigma(int_of_float (eval(e1, num)), int_of_float (eval(e2, num)), e3)
+		| INTEGRAL (e1, e2, e3) -> eval_integral(eval(e1, num), eval(e2, num), e3)
+and eval_sigma (n, e, expr) =
+	if(n > e) then 0.0
+	else eval(expr, float_of_int n) +. eval_sigma(n+1, e, expr)
+and eval_integral (n, e, expr) =
+	if(n > e) then -1.0 *. eval_integral(e, n, expr)
+	else if(e -. n < 0.1) then 0.0
+	else (0.1 *. eval(expr, n)) +. eval_integral(n +. 0.1, e, expr)
+	
 
+ 
 let rec galculator: exp -> float =
 	fun exp ->
 		match exp with
@@ -36,11 +38,11 @@ let rec galculator: exp -> float =
 		| INT num -> float_of_int num
 		| REAL num -> num
 		| ADD (e1, e2) -> (galculator e1) +. (galculator e2)
-		| SUB(e1, e2) -> (galculator e1) -. (galculator e2)
-		| MUL(e1, e2) -> (galculator e1) *. (galculator e2)
-		| DIV(e1, e2) -> (galculator e1) /. (galculator e2)
-		| SIGMA(e1, e2, e3) -> eval_sigma(e3, int_of_float(galculator e1), int_of_float(galculator e2))
-		| INTEGRAL(e1, e2, e3) -> eval_integral(e3, galculator e1, galculator e2)
+		| SUB (e1, e2) -> (galculator e1) -. (galculator e2)
+		| MUL (e1, e2) -> (galculator e1) *. (galculator e2)
+		| DIV (e1, e2) -> (galculator e1) /. (galculator e2)
+		| SIGMA (e1, e2, e3) -> eval_sigma((int_of_float (galculator e1)), (int_of_float (galculator e2)), e3)
+		| INTEGRAL (e1, e2, e3) -> eval_integral(galculator e1, galculator e2, e3)
 
 
 
