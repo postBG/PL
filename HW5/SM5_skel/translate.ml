ca17312feb69da2c87b6d0b5dc70d5d35959860a
postBG <profile2697@gmail.com>
2015-10-13 01:31:31 +0900

    first compile

--- HW5/SM5_skel/translate.ml
+++ HW5/SM5_skel/translate.ml
@@ -16,8 +16,8 @@ let rec inner_trans : K.program -> Sm5.command -> Sm5.command =
 		| K.NUM n -> Sm5.PUSH (Sm5.Val (Sm5.Z n))::cmds
 		| K.TRUE -> Sm5.PUSH (Sm5.Val (Sm5.B true))::cmds
 		| K.FALSE -> Sm5.PUSH (Sm5.Val (Sm5.B false))::cmds
-		| K.UNIT -> Sm5.PUSH (Sm5.Val Sm5.UNIT)::cmds
-		| K.VAR id -> Sm5.LOAD::(Sm5.PUSH (Sm5.ID id))::cmds
+		| K.UNIT -> Sm5.PUSH (Sm5.Val Sm5.Unit)::cmds
+		| K.VAR id -> Sm5.LOAD::(Sm5.PUSH (Sm5.Id id))::cmds
 		| K.ADD (e1, e2) ->
 			let e1_cmds = inner_trans e1 cmds in
 			let e2_cmds = inner_trans e2 e1_cmds in
@@ -47,17 +47,20 @@ let rec inner_trans : K.program -> Sm5.command -> Sm5.command =
 			Sm5.NOT::e_cmds
 		| K.ASSIGN (id, e) ->
 			let e_cmds = inner_trans e cmds in
-			let new_env_cmds = Sm5.STORE::(Sm5.PUSH (Sm5.ID id))::e_cmds in
-			Sm5.LOAD::(Sm5.PUSH (Sm5.ID id))::new_env_cmds
+			let new_env_cmds = Sm5.STORE::(Sm5.PUSH (Sm5.Id id))::e_cmds in
+			Sm5.LOAD::(Sm5.PUSH (Sm5.Id id))::new_env_cmds
 		| K.IF (cond, e1, e2) ->
 			let cond_cmds = inner_trans cond cmds in
 			let comp_then_cmds = List.rev (inner_trans e1 []) in
 			let comp_else_cmds = List.rev (inner_trans e2 []) in
 			(Sm5.JTR (comp_then_cmds, comp_else_cmds))::cond_cmds
+		| K.SEQ (e1, e2) ->
+			let e1_cmds = inner_trans e1 cmds in
+			inner_trans e2 e1_cmds
 		| K.LETV (id, e1, e2) ->
 			let e1_cmds = inner_trans e1 cmds in
-			let bind_cmds = (Sm5.Bind id)::Sm5.MALLOC::e1_cmds in
-			let store_cmds = (Sm5.STORE)::(Sm5.PUSH (Sm5.ID id))::bind_cmds in
+			let bind_cmds = (Sm5.BIND id)::Sm5.MALLOC::e1_cmds in
+			let store_cmds = (Sm5.STORE)::(Sm5.PUSH (Sm5.Id id))::bind_cmds in
 			let e2_cmds = inner_trans e2 store_cmds in
 			(* when end of use, remove this bind *)
 			Sm5.POP::Sm5.UNBIND::e2_cmds
@@ -65,9 +68,6 @@ let rec inner_trans : K.program -> Sm5.command -> Sm5.command =
 
 
 let rec trans : K.program -> Sm5.command = 
-	fun pgm -> 
-  		match pgm with
-    	| K.NUM n -> Sm5.empty_command (* Implement this. *)
-    	| _ -> Sm5.empty_command
+	fun pgm -> List.rev (inner_trans pgm [])
 
 end
