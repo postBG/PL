a8d52a6561c7e53878d9670399bd229c3826b040
postBG <profile2697@gmail.com>
2015-10-27 00:36:14 +0900

    compile okay, no test

--- HW6/Rozetta_skel/rozetta.ml
+++ HW6/Rozetta_skel/rozetta.ml
@@ -9,6 +9,10 @@ module Rozetta = struct
 
 	exception Error of string
 
+	let special_loc = (Sonata.Val (Sonata.L (-1, -1)))
+	let dummy_arg = (Sonata.Val (Sonata.Z 0))
+	let dummy_arg_loc = (Sonata.Val (Sonata.L (-2, -2)))
+
 	let rec trans_value : Sm5.value -> Sonata.value =
 		fun sm5_value ->
 			match sm5_value with
@@ -19,9 +23,10 @@ module Rozetta = struct
 			
 
 	(* when mode 1 -> need not return, mode 2 -> return *)
+	(* key invariant: #prev -> loc (-1, -1) -> (#prev_arg, removed C, E) *)
 	let rec inner_trans : Sm5.command -> int -> Sonata.command =
-		fun sm5_cmd mode ->
-			match sm5_cmd with
+		fun sm5_cmds mode ->
+			match sm5_cmds with
 			| (Sm5.PUSH obj)::tail -> 
 					(Sonata.PUSH (trans_obj obj))::(inner_trans tail mode)
 			| (Sm5.POP)::tail -> (Sonata.POP)::(inner_trans tail mode)
@@ -46,7 +51,13 @@ module Rozetta = struct
 			| (Sm5.LESS)::tail ->	(Sonata.LESS)::(inner_trans tail mode)
 			| (Sm5.NOT)::tail -> (Sonata.NOT)::(inner_trans tail mode)
 			| (Sm5.CALL)::tail -> raise (Error "not implemented")
-			|  _ -> raise (Error "not implemented")
+			| [] -> 
+				if (mode = 1) then
+					(Sonata.PUSH (Sonata.Id "#prev"))::(Sonata.LOAD)::
+						(Sonata.PUSH dummy_arg)::(Sonata.PUSH dummy_arg_loc)::
+							(Sonata.CALL)::[]
+				else []
+
 	and trans_obj : Sm5.obj -> Sonata.obj =
 		fun sm5_obj ->
 			match sm5_obj with
@@ -55,6 +66,14 @@ module Rozetta = struct
 			| Sm5.Fn (str, cmds) -> Sonata.Fn (str, (inner_trans cmds 1))
 
   let rec trans : Sm5.command -> Sonata.command = 
-  	fun command -> Sonata.empty_command (* Implement this. *)
+  	fun command -> (* set #prev for key invariant. #prev is always in env *)
+  		let end_fun = (Sonata.Fn ("#prev_arg", [])) in
+
+  		(Sonata.PUSH special_loc)::(Sonata.BIND "#prev")::
+  			(Sonata.PUSH end_fun)::(Sonata.PUSH (Sonata.Id "prev"))::(Sonata.STORE)::
+  				(inner_trans command 1)
+
+
+
 
 end
