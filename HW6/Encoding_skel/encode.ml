31406f616389c783ffda6255895fcfaa65254ee1
postBG <profile2697@gmail.com>
2015-10-29 20:28:44 +0900

    implemented easy things

--- HW6/Encoding_skel/encode.ml
+++ HW6/Encoding_skel/encode.ml
@@ -28,17 +28,18 @@ module Encoder =
 
 		let encode : M.mexp -> Lambda.lexp =
 			fun pgm -> 
-				| Num n -> (make_num n) 
-				| Var str -> raise (Error "not implemented") (* Implement this *)
-				| Fn (x, e) -> raise (Error "not implemented") (* Implement this *)
-				| App (e1, e2) -> raise (Error "not implemented") (* Implement this *)
-				| Ifz (e1, e2, e3) -> raise (Error "not implemented") (* Implement this *)
-				| Pair (e1, e2) -> raise (Error "not implemented") (* Implement this *)
-				| Fst e -> raise (Error "not implemented") (* Implement this *)
-				| Snd e -> raise (Error "not implemented") (* Implement this *)
-				| Add (e1, e2) -> raise (Error "not implemented") (* Implement this *)
-				| Sub (e1, e2) -> raise (Error "not implemented") (* Implement this *)
-				| And (e1, e2) -> raise (Error "not implemented") (* Implement this *)
-				| Rec (f, x, e) -> raise (Error "not implemented") (* Implement this *)
+				| Num n -> make_num n 
+				| Var str -> Lambda.Id str
+				| Fn (x, e) -> Lambda.Lam (x, encode e)
+				| App (e1, e2) -> Lambda.App (encode e1, encode e2)
+				| Rec (f, x, e) -> raise (Error "not implemented")
+				| Ifz (e1, e2, e3) -> raise (Error "not implemented") 
+				| Pair (e1, e2) -> raise (Error "not implemented") 
+				| Fst e -> raise (Error "not implemented") 
+				| Snd e -> raise (Error "not implemented") 
+				| Add (e1, e2) -> raise (Error "not implemented") 
+				| Sub (e1, e2) -> raise (Error "not implemented") 
+				| And (e1, e2) -> raise (Error "not implemented") 
+				| Rec (f, x, e) -> raise (Error "not implemented") 
 
  end
