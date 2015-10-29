71728c6cbca1ef382e8d525d9a409a70cb76f9f2
postBG <profile2697@gmail.com>
2015-10-29 19:13:09 +0900

    first commit

--- HW6/Encoding_skel/encode.ml
+++ HW6/Encoding_skel/encode.ml
@@ -8,7 +8,37 @@ module Encoder =
   struct
   	exception Error of string
 
-	let encode : M.mexp -> Lambda.lexp
-	= fun pgm -> raise (Error "not implemented") (* Implement this *)
+	  let rec inner_make_num : int -> Lambda.lexp =
+	  	fun n ->
+	  		if n < 0 then raise (Error "Negative exception")
+	  		else if n = 0 then (Lambda.Id "#x")
+	  		else
+	  			let f_form = inner_make_num (n-1) in
+	  			(Lambda.App (Lambda.Id "#f", f_form))
 
-  end
+
+	  let rec make_num : int -> Lambda.lexp =
+	  	fun n ->
+	  		let f_form = inner_make_num n in
+	  		Lambda.Lam ("#f", Lambda.Lam("#x", f_form))
+
+	  let rec true_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", "#x")))
+
+	  let rec false_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", "#y")))
+
+		let encode : M.mexp -> Lambda.lexp =
+			fun pgm -> 
+				| Num n -> (make_num n) 
+				| Var str -> raise (Error "not implemented") (* Implement this *)
+				| Fn (x, e) -> raise (Error "not implemented") (* Implement this *)
+				| App (e1, e2) -> raise (Error "not implemented") (* Implement this *)
+				| Ifz (e1, e2, e3) -> raise (Error "not implemented") (* Implement this *)
+				| Pair (e1, e2) -> raise (Error "not implemented") (* Implement this *)
+				| Fst e -> raise (Error "not implemented") (* Implement this *)
+				| Snd e -> raise (Error "not implemented") (* Implement this *)
+				| Add (e1, e2) -> raise (Error "not implemented") (* Implement this *)
+				| Sub (e1, e2) -> raise (Error "not implemented") (* Implement this *)
+				| And (e1, e2) -> raise (Error "not implemented") (* Implement this *)
+				| Rec (f, x, e) -> raise (Error "not implemented") (* Implement this *)
+
+ end
