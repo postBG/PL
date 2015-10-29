936b37cbebd2b177d454b4ba10b8035ca00574e0
postBG <profile2697@gmail.com>
2015-10-29 20:38:13 +0900

    fixed syntax error

--- HW6/Encoding_skel/encode.ml
+++ HW6/Encoding_skel/encode.ml
@@ -16,19 +16,19 @@ module Encoder =
 	  			let f_form = inner_make_num (n-1) in
 	  			(Lambda.App (Lambda.Id "#f", f_form))
 
-
 	  let rec make_num : int -> Lambda.lexp =
 	  	fun n ->
 	  		let f_form = inner_make_num n in
 	  		Lambda.Lam ("#f", Lambda.Lam("#x", f_form))
 
-	  let rec true_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", "#x")))
+	  let rec true_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", Lambda.Id "#x")))
 
-	  let rec false_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", "#y")))
+	  let rec false_of_bool = Lambda.Lam ("#x", (Lambda.Lam ("#y", Lambda.Id "#y")))
 
-		let encode : M.mexp -> Lambda.lexp =
-			fun pgm -> 
-				| Num n -> make_num n 
+		let rec encode : M.mexp -> Lambda.lexp =
+			fun pgm ->
+				match pgm with 
+				| Num n -> (make_num n)
 				| Var str -> Lambda.Id str
 				| Fn (x, e) -> Lambda.Lam (x, encode e)
 				| App (e1, e2) -> Lambda.App (encode e1, encode e2)
@@ -40,6 +40,6 @@ module Encoder =
 				| Add (e1, e2) -> raise (Error "not implemented") 
 				| Sub (e1, e2) -> raise (Error "not implemented") 
 				| And (e1, e2) -> raise (Error "not implemented") 
-				| Rec (f, x, e) -> raise (Error "not implemented") 
+				 
 
  end
