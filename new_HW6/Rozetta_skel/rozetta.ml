9fd7cc914e4a564c6d2872b07b67025a728bf924
postBG <profile2697@gmail.com>
2015-11-22 19:18:16 +0900

    refactor

--- new_HW6/Rozetta_skel/rozetta.ml
+++ new_HW6/Rozetta_skel/rozetta.ml
@@ -23,32 +23,32 @@ exception Error of string
 
   (* when mode 1 -> need not return, mode 2 -> return *)
   (* key invariant: #prev -> loc (-1, -1) -> (#prev_arg, removed C, E) *)
-  let rec inner_trans : Sm5.command -> int -> Sonata.command =
+  let rec trans' : Sm5.command -> int -> Sonata.command =
     fun sm5_cmds mode ->
       match sm5_cmds with
       | (Sm5.PUSH obj)::tail -> 
-          (Sonata.PUSH (trans_obj obj))::(inner_trans tail mode)
-      | (Sm5.POP)::tail -> (Sonata.POP)::(inner_trans tail mode)
-      | (Sm5.STORE)::tail -> (Sonata.STORE)::(inner_trans tail mode)
-      | (Sm5.LOAD)::tail -> (Sonata.LOAD)::(inner_trans tail mode)
+          (Sonata.PUSH (trans_obj obj))::(trans' tail mode)
+      | (Sm5.POP)::tail -> (Sonata.POP)::(trans' tail mode)
+      | (Sm5.STORE)::tail -> (Sonata.STORE)::(trans' tail mode)
+      | (Sm5.LOAD)::tail -> (Sonata.LOAD)::(trans' tail mode)
       | (Sm5.JTR (cmd1, cmd2))::tail ->
-          let sonata_cmd1 = inner_trans cmd1 2 in
-          let sonata_cmd2 = inner_trans cmd2 2 in
-          (Sonata.JTR (sonata_cmd1, sonata_cmd2))::(inner_trans tail mode)
-      | (Sm5.MALLOC)::tail -> (Sonata.MALLOC)::(inner_trans tail mode)
-      | (Sm5.BOX z)::tail -> (Sonata.BOX z)::(inner_trans tail mode)
-      | (Sm5.UNBOX x)::tail -> (Sonata.UNBOX x)::(inner_trans tail mode)
-      | (Sm5.BIND x)::tail -> (Sonata.BIND x)::(inner_trans tail mode)
-      | (Sm5.UNBIND)::tail -> (Sonata.UNBIND)::(inner_trans tail mode)
-      | (Sm5.GET)::tail -> (Sonata.GET)::(inner_trans tail mode)
-      | (Sm5.PUT)::tail -> (Sonata.PUT)::(inner_trans tail mode)
-      | (Sm5.ADD)::tail -> (Sonata.ADD)::(inner_trans tail mode)
-      | (Sm5.SUB)::tail -> (Sonata.SUB)::(inner_trans tail mode)
-      | (Sm5.MUL)::tail -> (Sonata.MUL)::(inner_trans tail mode)
-      | (Sm5.DIV)::tail -> (Sonata.DIV)::(inner_trans tail mode)
-      | (Sm5.EQ)::tail -> (Sonata.EQ)::(inner_trans tail mode)
-      | (Sm5.LESS)::tail -> (Sonata.LESS)::(inner_trans tail mode)
-      | (Sm5.NOT)::tail -> (Sonata.NOT)::(inner_trans tail mode)
+          let sonata_cmd1 = trans' cmd1 2 in
+          let sonata_cmd2 = trans' cmd2 2 in
+          (Sonata.JTR (sonata_cmd1, sonata_cmd2))::(trans' tail mode)
+      | (Sm5.MALLOC)::tail -> (Sonata.MALLOC)::(trans' tail mode)
+      | (Sm5.BOX z)::tail -> (Sonata.BOX z)::(trans' tail mode)
+      | (Sm5.UNBOX x)::tail -> (Sonata.UNBOX x)::(trans' tail mode)
+      | (Sm5.BIND x)::tail -> (Sonata.BIND x)::(trans' tail mode)
+      | (Sm5.UNBIND)::tail -> (Sonata.UNBIND)::(trans' tail mode)
+      | (Sm5.GET)::tail -> (Sonata.GET)::(trans' tail mode)
+      | (Sm5.PUT)::tail -> (Sonata.PUT)::(trans' tail mode)
+      | (Sm5.ADD)::tail -> (Sonata.ADD)::(trans' tail mode)
+      | (Sm5.SUB)::tail -> (Sonata.SUB)::(trans' tail mode)
+      | (Sm5.MUL)::tail -> (Sonata.MUL)::(trans' tail mode)
+      | (Sm5.DIV)::tail -> (Sonata.DIV)::(trans' tail mode)
+      | (Sm5.EQ)::tail -> (Sonata.EQ)::(trans' tail mode)
+      | (Sm5.LESS)::tail -> (Sonata.LESS)::(trans' tail mode)
+      | (Sm5.NOT)::tail -> (Sonata.NOT)::(trans' tail mode)
       | (Sm5.CALL)::tail -> 
           let set_up_rec_cmds = set_up_recursive tail in
           set_up_rec_cmds@[(Sonata.CALL)]
@@ -65,7 +65,7 @@ exception Error of string
       match sm5_obj with
       | Sm5.Val v -> Sonata.Val (trans_value v)
       | Sm5.Id str -> Sonata.Id str
-      | Sm5.Fn (str, cmds) -> Sonata.Fn (str, (inner_trans cmds 1))
+      | Sm5.Fn (str, cmds) -> Sonata.Fn (str, (trans' cmds 1))
   and set_up_recursive : Sm5.command -> Sonata.command = (* exchange box pointer *)  
     fun sm5_cmds -> 
       let store_prev_condition_cmds = store_prev_condition sm5_cmds in
@@ -80,7 +80,7 @@ exception Error of string
               (Sonata.PUSH (Sonata.Id store_prev_fun))::(Sonata.STORE)::[](* "store_prev_fun" => store_loc => [("prev", prev_loc)] *)
   and store_prev_condition : Sm5.command -> Sonata.command =
     fun sm5_cmds ->
-      let stored_cmds = (inner_trans sm5_cmds 1) in 
+      let stored_cmds = (trans' sm5_cmds 1) in 
       (Sonata.PUSH (Sonata.Id tmp_store_prev_fun))::(Sonata.LOAD)::
         (Sonata.PUSH (Sonata.Id store_prev_fun))::(Sonata.STORE)::(* recover box first *)
           (Sonata.UNBIND)::(Sonata.POP)::
@@ -100,4 +100,4 @@ exception Error of string
               (Sonata.BOX 1)::(* [("prev", prev_loc)]::S *)
               (Sonata.MALLOC)::(Sonata.BIND store_prev_fun)::(*"store_prev_fun" => store_loc*)
                 (Sonata.PUSH (Sonata.Id store_prev_fun))::(Sonata.STORE)::(* "store_prev_fun" => store_loc => [("prev", prev_loc)] *)
-                (inner_trans command 1)
+                (trans' command 1)
